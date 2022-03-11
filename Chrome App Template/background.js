35dd02b15c00a2518636ed998740378326436634
04 <101374286+04-0@users.noreply.github.com>
2022-03-10 19:19:41 -0700

    Update background.js

--- Chrome App Template/background.js
+++ Chrome App Template/background.js
@@ -1,6 +1,12 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
     'outerBounds': {
+      
+      // put everything in here, or just put a function above that
+      // calls from 'onlaunched'
+      
+      // when run, sets dimensions that are resizable
+      
       'width': 400,
       'height': 500
     }
